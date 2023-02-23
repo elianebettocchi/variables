733fca8a371fb38c04a3f6269d6bea6f221abbbf
elianebettocchi <116610216+elianebettocchi@users.noreply.github.com>
2023-02-22 19:44:55 -0500

    Add files via upload

---
+++ test.js
@@ -0,0 +1,47 @@
+var a = 23;
+var b = 47;
+var c = 89;
+var d = 64;
+
+function sum(a,b){
+  var sum = (a+b);
+  return sum;
+  console.log(a + b);
+}
+sum(a,b);
+
+function subtract(a,d){
+  var subtract = (a-d);
+  return subtract;
+  console.log(a - d);
+}
+subtract(a,d);
+
+function product(b,c){
+  var product = (b*c);
+  return product;
+  console.log(b * c);
+}
+product(b,c);
+
+function divide(b,d){
+  var divide = (b/d);
+  return divide;
+  console.log(b / d);
+}
+divide(b,d);
+
+function increment(a){
+  var increment = (a++);
+  return increment;
+  console.log(a);
+}
+increment(a);
+
+function decrement(d){
+  var decrement = (d--);
+  return decrement;
+  console.log(d);
+}
+decrement(d);
+
